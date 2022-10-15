3ce4b97ed868500a707af5a9a8e011eff8d2655f
Md-Alqma <mdalqma7@gmail.com>
2022-10-15 13:52:59 +0530

    added some fuctionality and created some more sections like - course description and price

--- Product-Desc/pro_desc.js
+++ Product-Desc/pro_desc.js
@@ -1,39 +1,40 @@
 let hidDiv = document.querySelector('.hiddenParent');
 
 document.querySelector('.seeMoreBtn').onclick = () => {
-    // console.log("sfdsdf");
-    // let tickDiv = document.createElement("div");
-    // tickDiv.setAttribute("class", "tick-sign");
-    // let tick = document.createElement('span');
-    // tick.innerHTML = '✓';
-    // let pDIv = document.createElement('div');
-    // pDIv.setAttribute('class', 'desc');
-    // let pDesc = document.createElement('p');
-    // pDesc.innerHTML = "Use CSS Frameworks including Bootstrap 5, Semantic UI, and Bulma";
     
     hidDiv.style.display = 'flex';
     document.querySelector('.seeMoreBtn').style.display = 'none';
     document.querySelector('.showLess').style.display = 'block';
-    // document.querySelector(".showLess").classList.remove('.seeMoreBtn');
-    // let showLessBtn = document.querySelector('.seeMoreBtn');
-    // showLessBtn.innerHTML = "See Less";
-    // showLessBtn.setAttribute('class', 'showLess');
-
-    // showLessBtn.onclick = () => { 
-    //     hidFDiv.style.display = 'none';
-    //     showLessBtn.innerHTML = "Show More";
-        // showLessBtn.onclick = () => { 
-        //     expand();
-        // }
-        // expand();
-    // }
 }
 
 document.querySelector('.showLess').onclick = () => {
+    document.querySelector('.seeMoreBtn').style.display = 'block';
+    document.querySelector('.showLess').style.display = 'none';
     hidDiv.style.display = 'none';
-    document.querySelector('.showLess').innerHTML = "Show More";
-    // expand();
-    // document.querySelector(".seeMoreBtn").classList.remove('.showLess');
-    // document.querySelector('.showLess').setAttribute('.seeMoreBtn');
-    // expand();
 }
+
+
+const preview = () => { 
+
+}
+
+let hiddenDescription = document.querySelector('.hiddenDescription');
+
+document.querySelector('.showDesc').onclick = () => {
+    hiddenDescription.style.display = 'block';
+    document.querySelector('.hideDesc').style.display = 'block';
+    document.querySelector('.showDesc').style.display = 'none';
+
+    
+    document.querySelector('.hideDesc').onclick = () => { 
+        document.querySelector('.hideDesc').style.display = 'none';
+        document.querySelector('.showDesc').style.display = 'block';
+        hiddenDescription.style.display = 'none';
+    }
+
+}
+
+// document.querySelector('#prevPrice') = () => {
+//     document.querySelector('.previewBox').style.display = 'none';
+//     // document.querySelector('body').
+// }
